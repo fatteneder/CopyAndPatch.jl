541a717f67adf063f7d69bbed180a39e7b2c70c3
Florian Atteneder <florian.atteneder@gmail.com>
2024-02-22 22:07:58 +0100

    remove unused

--- stencils/mwes/hex2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-float x[1] = {1.1};
-
-int main() {
-    printf("x = %f\n", x[0]);
-    return 0;
-}
--- stencils/mwes/libhex.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-
-double hex(double x) {
-    printf("%016x\n", x);
-    return x;
-}
