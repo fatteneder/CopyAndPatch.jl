99eae2be2a5020c8fab7404416c82779a0551692
Florian Atteneder <florian.atteneder@gmail.com>
2024-02-25 00:09:37 +0100

    add box/unbox methods for debugging

--- src/utils.jl
+++ src/utils.jl
@@ -8,3 +8,24 @@ function pointer_from_function(fn::Function)
     @assert pf !== C_NULL
     return pf
 end
+
+
+# for debugging jl_call
+box(x::Int16) = ccall((:jl_box_int16,path_libjulia[]), Ptr{Cvoid}, (Int16,), x)
+box(x::Int32) = ccall((:jl_box_int32,path_libjulia[]), Ptr{Cvoid}, (Int32,), x)
+box(x::Int64) = ccall((:jl_box_int64,path_libjulia[]), Ptr{Cvoid}, (Int64,), x)
+box(x::UInt16) = ccall((:jl_box_uint16,path_libjulia[]), Ptr{Cvoid}, (UInt16,), x)
+box(x::UInt32) = ccall((:jl_box_uint32,path_libjulia[]), Ptr{Cvoid}, (UInt32,), x)
+box(x::UInt64) = ccall((:jl_box_uint64,path_libjulia[]), Ptr{Cvoid}, (UInt64,), x)
+box(x::Float16) = ccall((:jl_box_float16,path_libjulia[]), Ptr{Cvoid}, (Float16,), x)
+box(x::Float32) = ccall((:jl_box_float32,path_libjulia[]), Ptr{Cvoid}, (Float32,), x)
+box(x::Float64) = ccall((:jl_box_float64,path_libjulia[]), Ptr{Cvoid}, (Float64,), x)
+unbox(::Type{Int16}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_int16,path_libjulia[]), Int16, (Ptr{Cvoid},), ptr)
+unbox(::Type{Int32}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_int32,path_libjulia[]), Int32, (Ptr{Cvoid},), ptr)
+unbox(::Type{Int64}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_int64,path_libjulia[]), Int64, (Ptr{Cvoid},), ptr)
+unbox(::Type{UInt16}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_uint16,path_libjulia[]), UInt16, (Ptr{Cvoid},), ptr)
+unbox(::Type{UInt32}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_uint32,path_libjulia[]), UInt32, (Ptr{Cvoid},), ptr)
+unbox(::Type{UInt64}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_uint64,path_libjulia[]), UInt64, (Ptr{Cvoid},), ptr)
+unbox(::Type{Float16}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_float16,path_libjulia[]), Float16, (Ptr{Cvoid},), ptr)
+unbox(::Type{Float32}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_float32,path_libjulia[]), Float32, (Ptr{Cvoid},), ptr)
+unbox(::Type{Float64}, ptr::Ptr{Cvoid}) = ccall((:jl_unbox_float64,path_libjulia[]), Float64, (Ptr{Cvoid},), ptr)
