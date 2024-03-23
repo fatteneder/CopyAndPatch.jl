1c09bac4d02bb12d8c34c1af0bc9aa81a21c3545
Florian Atteneder <florian.atteneder@gmail.com>
2024-03-23 22:19:43 +0100

    update mwe_jl_add_int.jl to test a jl_add_int.jl stencil

--- stencils/mwes/mwe_jl_add_int.jl
+++ stencils/mwes/mwe_jl_add_int.jl
@@ -1,14 +1,42 @@
 using CopyAndPatch
+CopyAndPatch.init_stencils()
 
 
 # can call the runtime intrinsics from libjulia-internal
-a, b = Int8(1), Int8(2)
+T = Int64
+a, b = T(2), T(3)
 box_a, box_b = CopyAndPatch.box(a), CopyAndPatch.box(b)
 box_ret = ccall((:jl_add_int,CopyAndPatch.path_libjuliainternal[]), Ptr{Cvoid},
                 (Ptr{Cvoid},Ptr{Cvoid}),
                 box_a, box_b)
-ret = CopyAndPatch.unbox(Int8,box_ret)
-box_ret = ccall((:jl_mul_int,CopyAndPatch.path_libjuliainternal[]), Ptr{Cvoid},
-                (Ptr{Cvoid},Ptr{Cvoid}),
-                box_a, box_b)
-ret = CopyAndPatch.unbox(Int8,box_ret)
+@show CopyAndPatch.unbox(T,box_ret)
+# box_ret = ccall((:jl_mul_int,CopyAndPatch.path_libjuliainternal[]), Ptr{Cvoid},
+#                 (Ptr{Cvoid},Ptr{Cvoid}),
+#                 box_a, box_b)
+# @show CopyAndPatch.unbox(T,box_ret)
+
+# # this segfaults when we don't use the Core.IntrinsicFunction dispatch for pointer_from_function
+# # however, that one does not work reliably as was clarified on slack
+# fptr = CopyAndPatch.pointer_from_function(Base.add_int)#, :add_int)
+# @show fptr, hash(Base.add_int)
+# box_ret = ccall(fptr, Ptr{Cvoid},
+#                 (Ptr{Cvoid},Ptr{Cvoid}),
+#                 box_a, box_b)
+# @show CopyAndPatch.unbox(T,box_ret)
+# # fptr = CopyAndPatch.pointer_from_function(Base.mul_int, :mul_int)
+# # box_ret = ccall(fptr, Ptr{Cvoid},
+# #                 (Ptr{Cvoid},Ptr{Cvoid}),
+# #                 box_a, box_b)
+# # @show CopyAndPatch.unbox(T,box_ret)
+
+ret = Ref{Ptr{Cvoid}}(C_NULL)
+_, jitend, _ = CopyAndPatch.stencils["jit_end"]
+_, jl_add_int, _ = CopyAndPatch.stencils["jl_add_int"]
+stack = Ptr{Cvoid}[ pointer(jitend), Base.unsafe_convert(Ptr{Cvoid},ret), box_b, box_a, pointer(jl_add_int) ]
+
+entry = stack[end]
+@show pointer(jitend)
+@show ret
+ccall(entry, Cvoid, (Ptr{Cvoid},), pointer(stack, length(stack)-1)) # -1 because we removed entry
+@show ret
+@show CopyAndPatch.unbox(T,ret[])
