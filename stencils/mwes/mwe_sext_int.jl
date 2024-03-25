9a39380d5c0f3243eb99544015519e8278dfc3b3
Florian Atteneder <florian.atteneder@gmail.com>
2024-03-25 21:54:07 +0100

    add mwe_sext_int.jl

---
+++ stencils/mwes/mwe_sext_int.jl
@@ -0,0 +1,16 @@
+using CopyAndPatch
+CopyAndPatch.init_stencils()
+
+
+T = Int64
+val = Int32(123)
+ret = Ref{Ptr{Cvoid}}(C_NULL)
+_, jitend, _ = CopyAndPatch.stencils["jit_end"]
+_, jl_sext_int, _ = CopyAndPatch.stencils["jl_sext_int"]
+stack = Ptr{Cvoid}[ pointer(jitend), # return
+                   Base.unsafe_convert(Ptr{Cvoid},ret), CopyAndPatch.box(val), pointer_from_objref(T), pointer(jl_sext_int) # box call
+                   ]
+entry = stack[end]
+ccall(entry, Cvoid, (Ptr{Cvoid},), pointer(stack, length(stack)-1)) # -1, because we popped entry
+@show ret
+@show CopyAndPatch.unbox(T,ret[])
