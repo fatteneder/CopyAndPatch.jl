818af2b49c20c3846624c7a58db5cbe9abea426a
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-02 21:31:02 +0200

    add voidy host to setup.sh

--- stencils/setup.sh
+++ stencils/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-export PATH="$HOME/llvm-project/build/bin:$HOME/wd/julia/usr/bin:$PATH"
-export JULIA_INCLUDE="$HOME/wd/julia2/usr/include/julia"
-export JULIA_INCLUDE2="$HOME/wd/julia2/usr/include"
-export JULIA_INTERNAL_INCLUDE="$HOME/wd/julia2/src"
+if [[ $(hostname) == "voidy" ]]; then
+  export PATH="$HOME/llvm-project/build/bin:$PATH"
+  export PATH="$HOME/wd/julia2/usr/bin:$PATH"
+  export JULIA_DEPOT_PATH="$HOME/wd/julia2/usr/share/julia"
+  export JULIA_INCLUDE="$HOME/wd/julia2/usr/include/julia"
+  export JULIA_INCLUDE2="$HOME/wd/julia2/usr/include"
+  export JULIA_INTERNAL_INCLUDE="$HOME/wd/julia2/src"
+fi
