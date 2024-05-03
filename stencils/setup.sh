e72fac52bd76775e0395186d77571543902785fe
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-03 13:48:44 +0200

    add fwork setup config

--- stencils/setup.sh
+++ stencils/setup.sh
@@ -7,4 +7,10 @@ if [[ $(hostname) == "voidy" ]]; then
   export JULIA_INCLUDE="$HOME/wd/julia2/usr/include/julia"
   export JULIA_INCLUDE2="$HOME/wd/julia2/usr/include"
   export JULIA_INTERNAL_INCLUDE="$HOME/wd/julia2/src"
+elif [[ $(hostname) == "fwork" ]]; then
+  export PATH="$HOME/binaries/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04/bin:$PATH"
+  export PATH="$HOME/wd/julia2/usr/bin:$PATH"
+  export JULIA_INCLUDE="$HOME/wd/julia2/usr/include/julia"
+  export JULIA_INCLUDE2="$HOME/wd/julia2/usr/include"
+  export JULIA_INTERNAL_INCLUDE="$HOME/wd/julia2/src"
 fi
