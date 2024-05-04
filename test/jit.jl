7342e9c0f37dda392ddc6cc9360fffd2b36e18d7
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-04 18:53:55 +0200

    gotoifnot test

--- test/jit.jl
+++ test/jit.jl
@@ -1,5 +1,4 @@
 @testset "intrinsics" begin
-
     f1(x) = x+2
     f2(x) = (x+2)*3-x^3
     f3(x) = (x+2)/3
@@ -7,7 +6,6 @@
         versioninfo()
         (x+2)/3
     end
-
     for T in (Int64,Int32), f in (f1,f2,f3)
         @test try
             memory = jit(f, (T,))
@@ -18,7 +16,6 @@
             false
         end
     end
-
 end
 
 @noinline function g(x,y)
@@ -27,14 +24,12 @@ end
 end
 # TODO Moving f(x) to here gives a segfault
 @testset "multiple calls" begin
-
     function f(x)
         versioninfo()
         g(x,2*x)
         x += log(x)
         (x+2)/3
     end
-
     for T in (Int64,Int32)
         @test try
             memory = jit(f, (T,))
@@ -45,5 +40,18 @@ end
             false
         end
     end
+end
 
+@testset "GotoIfNot" begin
+    f(x) = x > 1 ? 1 : 2
+    for T in (Int64,Int32)
+        @test try
+            memory = jit(f, (T,))
+            ccall(pointer(memory), Cvoid, (Ptr{Cvoid},), C_NULL)
+            true
+        catch e
+            @error "Failed $f(::$T) with" e
+            false
+        end
+    end
 end
