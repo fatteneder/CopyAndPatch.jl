10d52b84c1dccbaedac44a9c90d073b88c5b157b
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-07 21:26:15 +0200

    fixup clang complains about gotoifnot stencil

--- stencils/jit_gotoifnot.c
+++ stencils/jit_gotoifnot.c
@@ -3,9 +3,10 @@
 void
 _JIT_ENTRY(int ip) {
    PATCH_VALUE(jl_value_t *, test, _JIT_TEST);
-   if (test == jl_false)
+   if (test == jl_false) {
       PATCH_JUMP(_JIT_CONT1, ip+1);
-   else if (test != jl_true)
+   } else if (test != jl_true) {
       jl_type_error("if", (jl_value_t*)jl_bool_type, test);
+   }
    PATCH_JUMP(_JIT_CONT2, ip+1);
 }
