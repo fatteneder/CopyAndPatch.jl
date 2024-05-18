1ba0a6290df0d0e5a55b5131f0f0070db4b259c1
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-19 00:55:42 +0200

    insert method instance assertion

--- stencils/ast_invoke.c
+++ stencils/ast_invoke.c
@@ -9,9 +9,9 @@ _JIT_ENTRY(int prev_ip)
    PATCH_VALUE(jl_value_t **,  ret,     _JIT_RET);
    PATCH_VALUE(int,            retused, _JIT_RETUSED);
    jl_method_instance_t *meth = (jl_method_instance_t*)(*(args[0]));
-   // TODO Insert the assertion again!!!
+   assert(jl_is_method_instance(meth));
    jl_value_t **argv;
-   JL_GC_PUSHARGS(argv, nargs - 1);
+   JL_GC_PUSHARGS(argv, nargs-1);
    for (size_t i = 1; i < nargs; i++)
       argv[i-1] = *(args[i]);
    jl_value_t *_ret = jl_invoke(argv[0], nargs == 2 ? NULL : &argv[1], nargs-2, meth);
