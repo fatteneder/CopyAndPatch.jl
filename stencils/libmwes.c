d518348a504cb355147b3b638a7015da12bb40fd
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-20 21:07:02 +0200

    add more helpers to libmwes

--- stencils/libmwes.c
+++ stencils/libmwes.c
@@ -5,9 +5,26 @@
 #include <stdlib.h>
 
 
-jl_value_t * mwe_jl_alloc_genericmemory(size_t nel) {
+jl_value_t * mwe_jl_alloc_genericmemory_carg(size_t nel) {
    jl_value_t *memory_type = jl_eval_string("Memory{Int64}");
    jl_value_t *memory = (jl_value_t *)jl_alloc_genericmemory(memory_type, nel);
+   printf("memory_type = %p\n", memory_type);
+   printf("memory = %p\n", memory);
+   printf("jl_typeof_str(memory) = %s\n", jl_typeof_str(memory));
+   return memory;
+}
+
+jl_value_t * mwe_jl_alloc_genericmemory_jlarg(jl_value_t *memory_type) {
+   jl_value_t *memory_type_from_eval = jl_eval_string("Memory{Int64}");
+   size_t nel = 3;
+   printf("memory_type = %p\n", memory_type);
+   printf("memory_type_from_eval = %p\n", memory_type_from_eval);
+   printf("jl_typeof_str(memory_type) = %s\n", jl_typeof_str(memory_type));
+   printf("jl_typeof_str(memory_type_from_eval) = %s\n", jl_typeof_str(memory_type_from_eval));
+   jl_value_t *memory = (jl_value_t *)jl_alloc_genericmemory(memory_type, nel);
+   printf("memory = %p\n", memory);
+   printf("jl_typeof_str(memory) = %s\n", jl_typeof_str(memory));
+   fflush(stdout);
    return memory;
 }
 
@@ -38,4 +55,43 @@ int * mwe_alloc_an_array(int n) {
    for (int i = 0; i < n; i++)
       a[i] = i;
    return a;
+}
+
+jl_value_t * get_tuple_pointer() {
+   jl_value_t *tpl = jl_eval_string("(2,)");
+   printf("jl_is_tuple(tpl) = %d\n", jl_is_tuple(tpl));
+   return tpl;
+}
+
+int64_t mwe_foreign_carg_cret(int64_t n) {
+   printf("n = %d\n", n);
+   return 1;
+}
+
+int64_t mwe_foreign_cptr_cret(int64_t *n) {
+   printf("n = %p\n", n);
+   printf("n[1] = %d\n", n[1]);
+   return 1;
+}
+
+int64_t mwe_foreign_jlarg_cret(jl_value_t *n) {
+   printf("n = %p\n", n);
+   printf("jl_typeof_str(n) = ");
+   printf("%s\n", jl_typeof_str(n));
+   return 1;
+}
+
+jl_value_t * mwe_foreign_carg_jlret(int64_t n) {
+   printf("n = %d\n", n);
+   jl_value_t *v = jl_eval_string("1");
+   return v;
+   /** return 1; */
+}
+
+jl_value_t * mwe_foreign_jlarg_jlret(jl_value_t *n) {
+   printf("n = %p\n", n);
+   printf("jl_typeof_str(n) = %s\n", jl_typeof_str(n));
+   jl_value_t *v = jl_eval_string("1");
+   return v;
+   /** return 1; */
 }
