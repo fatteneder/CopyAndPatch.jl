aaf6263e9737a740e63c9ac2079e000397544734
Florian Atteneder <florian.atteneder@gmail.com>
2024-05-29 09:23:54 +0200

    format [JITDEBUG]

--- stencils/common.h
+++ stencils/common.h
@@ -15,9 +15,16 @@ do {                               \
     return (ALIAS)(IP);            \
 } while (0);
 
+#define RESET_COLOR   "\033[39m"
+#define FG_GREEN      "\033[32m"
+#define FG_YELLOW     "\033[33m"
+#define RESET_FORMAT  "\033[0m"
+#define BOLD          "\033[1m"
+
 #ifdef JITDEBUG
     #define DEBUGSTMT(NAME, IP) \
-        printf("[JITDEBUG] ip %-4d reached " NAME "\n", (IP)); \
+        printf(BOLD FG_YELLOW "[" FG_GREEN "JITDEBUG" FG_YELLOW "]" RESET_COLOR RESET_FORMAT \
+               " ip %-4d reached " NAME "\n", (IP)); \
         fflush(stdout)
 #else
     #define DEBUGSTMT(NAME, IP)
