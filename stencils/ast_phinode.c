735d0907240eb757e44c9a90b9843f8ffd5ceab3
Florian Atteneder <florian.atteneder@gmail.com>
2024-06-30 17:25:41 +0200

    handling of phinodes appearing in blocks
    This is handled with a special interpreter eval_phi in interpreter.c.
    We slightly deviate from interpreter.c and keep one phinode stencil
    per phinode. Instead we chain those nodes by forwarding the prev_ip
    in a phinode block.

--- stencils/ast_phinode.c
+++ stencils/ast_phinode.c
@@ -3,16 +3,17 @@
 void
 _JIT_ENTRY(int prev_ip)
 {
-   PATCH_VALUE(int *,          edges,   _JIT_EDGES);
-   PATCH_VALUE(int,            ip,      _JIT_IP);
-   PATCH_VALUE(int,            nedges,  _JIT_NEDGES);
-   PATCH_VALUE(jl_value_t **,  ret,     _JIT_RET);
-   PATCH_VALUE(jl_value_t ***, vals,    _JIT_VALS);
+   PATCH_VALUE(int *,          edges,     _JIT_EDGES);
+   PATCH_VALUE(int,            ip,        _JIT_IP);
+   PATCH_VALUE(int,            chain_phi, _JIT_CHAIN_PHI);
+   PATCH_VALUE(int,            nedges,    _JIT_NEDGES);
+   PATCH_VALUE(jl_value_t **,  ret,       _JIT_RET);
+   PATCH_VALUE(jl_value_t ***, vals,      _JIT_VALS);
    DEBUGSTMT("ast_phinode", prev_ip, ip);
    for (int ie = 0; ie < nedges; ie++) {
       if (edges[ie] == prev_ip) {
          *ret = *(vals[ie]);
-         PATCH_JUMP(_JIT_CONT, ip);
+         PATCH_JUMP(_JIT_CONT, chain_phi ? prev_ip : ip);
       }
    }
    jl_error("ast_phinode: This should not have happened!");
