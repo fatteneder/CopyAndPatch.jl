78f6560807e3650f33d27841fb1c217b1bf3fa31
Florian Atteneder <florian.atteneder@gmail.com>
2024-10-31 11:27:00 +0100

    update setup.sh

--- stencils/setup.sh
+++ stencils/setup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 if [[ $(hostname) == "voidy" ]]; then
+  JULIA_ROOT="$HOME/wd/julia2"
   export PATH="$HOME/llvm-project/build/bin:$PATH"
-  export PATH="$HOME/wd/julia2/usr/bin:$PATH"
-  export JULIA_DEPOT_PATH="$HOME/wd/julia2/usr/share/julia"
-  export JULIA_INCLUDE="$HOME/wd/julia2/usr/include"
-  export JULIA_LIB="$HOME/wd/julia2/usr/lib"
-  export JULIA_INTERNAL_INCLUDE="$HOME/wd/julia2/src"
+  export PATH="$JULIA_ROOT/usr/bin:$PATH"
+  export JULIA_DEPOT_PATH="$JULIA_ROOT/usr/share/julia"
+  export JULIA_INCLUDE="$JULIA_ROOT/usr/include"
+  export JULIA_LIB="$JULIA_ROOT/usr/lib"
+  export JULIA_INTERNAL_INCLUDE="$JULIA_ROOT/src"
   export LIBFFI_INCLUDE="/usr/lib64"
 elif [[ $(hostname) == "fwork" ]]; then
+  JULIA_ROOT="$HOME/wd/julia3"
   export PATH="$HOME/binaries/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04/bin:$PATH"
-  export PATH="$HOME/wd/julia-prot-exec/usr/bin:$PATH"
-  export JULIA_INCLUDE="$HOME/wd/julia-prot-exec/usr/include"
-  export JULIA_LIB="$HOME/wd/julia-prot-exec/usr/lib"
-  export JULIA_INTERNAL_INCLUDE="$HOME/wd/julia-prot-exec/src"
+  export PATH="$JULIA_ROOT/usr/bin:$PATH"
+  export JULIA_INCLUDE="$JULIA_ROOT/usr/include"
+  export JULIA_LIB="$JULIA_ROOT/usr/lib"
+  export JULIA_INTERNAL_INCLUDE="$JULIA_ROOT/src"
   export LIBFFI_INCLUDE="/usr/lib64"
 fi
