128b25afb2623ce433a9657605f3a8606685dadf
Florian Atteneder <florian.atteneder@gmail.com>
2024-11-01 21:05:07 +0100

    fix ast_boundscheck.c

--- stencils/ast_boundscheck.c
+++ stencils/ast_boundscheck.c
@@ -5,8 +5,7 @@ _JIT_ENTRY(int prev_ip)
 {
    PATCH_VALUE(int,     ip,  _JIT_IP);
    PATCH_VALUE(void **, ret, _JIT_RET);
-   PATCH_VALUE(void **, val, _JIT_VAL);
    DEBUGSTMT("ast_boundscheck", prev_ip, ip);
-   *ret = *val;
+   *ret = jl_true;
    PATCH_JUMP(_JIT_CONT, ip);
 }
