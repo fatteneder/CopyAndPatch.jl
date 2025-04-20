0a499d45d9eab4bdddce05bf3cbb8264834a9f81
Florian Atteneder <florian.atteneder@gmail.com>
2025-04-20 19:53:00 +0200

    add ast_apply stencil

---
+++ stencils/ast_apply.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+JIT_ENTRY()
+{
+   PATCH_VALUE(int,      ip,    _JIT_IP); // 1-based
+   PATCH_VALUE(uint32_t, nargs, _JIT_NARGS);
+   DEBUGSTMT("ast_apply", F, ip);
+   F->ssas[ip-1] = jl_apply(F->tmps, nargs);
+   SET_IP(F, ip);
+   PATCH_JUMP(_JIT_CONT, F);
+}
